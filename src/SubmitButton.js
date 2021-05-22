73b549309e824427bf64b0a065daa5dcf58309af
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-05-22 13:41:46 -0700

    added a component submitForm

---
+++ src/SubmitButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class SubmitButton extends React.Component {
+  render() {
+    return (
+      <div className="submitButton">
+        Hello World
+      </div>
+    );
+  }
+  
+}
+
+export default SubmitButton;
