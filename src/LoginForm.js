8e31d7bf137ec3f812c22ecc598cf4ea6e266b3f
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-05-22 13:39:01 -0700

    added a component LoginForm

---
+++ src/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <div className="loginForm">
+        Hello World
+      </div>
+    );
+  }
+  
+}
+
+export default LoginForm;
